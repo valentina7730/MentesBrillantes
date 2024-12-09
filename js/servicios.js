22daeaafb7918b2fdca0514e3b3cc42661883fed
MikeSSM <trabajoprogrammer@gmail.com>
2024-12-08 23:00:17 -0500

    Actualización de css, grid de las imágenes en el carrusel de imágenes

--- js/servicios.js
+++ js/servicios.js
@@ -11,7 +11,7 @@ function loadShow(){
     items[active].style.opacity = 1;
     for(var i  = active + 1; i< items.length; i++){
         stt++;
-        items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.transform = `translateX(${180*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
         items[i].style.zIndex= -stt;
         items[i].style.filter = 'blur(5px)';
         items[i].style.opacity = stt > 2 ? 0 : 0.6;
